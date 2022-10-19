64ef4a4bfb329674eccd7c1c5e74b83f390aee80
song-cool <oscar2978@naver.com>
2022-10-19 23:25:04 +0900

    feat : 네비게이션 상태를 확인하는 변수를 만들었습니다.
    isNavigationActivated : 네비게이션이 활성화 되었는가?

--- Rlog/View/WorkSpace/WorkSpaceCreateScheduleListView.swift
+++ Rlog/View/WorkSpace/WorkSpaceCreateScheduleListView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct WorkSpaceCreateScheduleListView: View {
     
+    @State var isNavigationActivated: Bool = false
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
             //타이틀 섭뷰 넣기 "근무 일정을 입력해주세요." Or "근무 유형을 추가해주세요!"
