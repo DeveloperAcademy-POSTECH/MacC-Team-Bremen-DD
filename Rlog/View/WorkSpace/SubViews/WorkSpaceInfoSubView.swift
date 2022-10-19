57222396d9cb89f1db3e3f0f8e7a37b3f51d8fa3
song-cool <oscar2978@naver.com>
2022-10-19 22:33:04 +0900

    feat : 근무지 정보를 담는 섭뷰를 만들었습니다.

---
+++ Rlog/View/WorkSpace/SubViews/WorkSpaceInfoSubView.swift
@@ -0,0 +1,24 @@
+//
+//  WorkSpaceInfoSubView.swift
+//  Rlog
+//
+//  Created by 송시원 on 2022/10/19.
+//
+
+import SwiftUI
+
+struct WorkSpaceInfoSubView: View {
+    let labelName: String
+    let content: String
+    
+    var body: some View {
+
+        VStack(alignment: .leading, spacing: 10) {
+            Text(labelName)
+                .font(.caption)
+                .foregroundColor(.fontLightGray)
+            Text(content)
+                .foregroundColor(.fontBlack)
+        }
+    }
+}
