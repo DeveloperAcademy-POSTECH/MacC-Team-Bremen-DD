1536c50f35acf7bf7ea8afe21eca35761a32ed83
Junghoon-P <77766769+Junghoon-P@users.noreply.github.com>
2022-10-20 22:28:58 +0900

    refactor : BD-31 근무지 카드 디테일링 수정

--- Rlog/View/WorkSpace/WorkSpaceListView.swift
+++ Rlog/View/WorkSpace/WorkSpaceListView.swift
@@ -7,62 +7,7 @@
 
 import SwiftUI
 
-struct WorkSpaceListView: View {
-    
-    var body: some View {
-        NavigationView {
-            ScrollView {
-                ForEach(models, id: \.self) { model in
-                    NavigationLink(
-                        destination: { WorkSpaceDetailView() },
-                        label: { WorkSpaceCardContents(model: model) }
-                    )
-                }
-            }
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Text("근무지")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: { print("button pressed") }){
-                        Image(systemName: "plus")
-                    }
-                }
-            }
-        }
-    }
-}
-
-private extension WorkSpaceListView {
-    func WorkSpaceCardContents(model: CustomModel) -> some View {
-        VStack(alignment:. leading) {
-            Text(model.title)
-            
-            ForEach(Info.allCases, id: \.self) { tab in
-                HStack {
-                    Text(tab.text)
-                    Spacer()
-                    Text(model.text(info: tab))
-                }
-            }
-        }
-        .padding()
-        .background(Color.blue.opacity(0.1))
-        .padding([.horizontal, .bottom])
-    }
-    
-}
-
-struct WorkSpaceListView_Previews: PreviewProvider {
-    static var previews: some View {
-        WorkSpaceListView()
-    }
-}
-
-enum Info: CaseIterable {
+enum WorkSpaceInfo: CaseIterable {
     case money
     case date
     case pay
@@ -80,6 +25,87 @@ enum Info: CaseIterable {
     }
 }
 
+struct WorkSpaceListView: View {
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.cardBackground
+                
+                ScrollView {
+                    ForEach(models, id: \.self) { model in
+                        NavigationLink(
+                            destination: { WorkSpaceDetailView() },
+                            label: { WorkSpaceCardContents(model: model) }
+                        )
+                    }
+                }
+                .padding(.top, 32)
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Text("근무지")
+                            .font(.title)
+                            .fontWeight(.bold)
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(action: { print("button pressed") }){
+                            Image(systemName: "plus")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+private extension WorkSpaceListView {
+    func WorkSpaceCardHeader(workTitle: String) -> some View {
+        HStack(alignment: .center){
+            Rectangle()
+                .fill(.primary)
+                .frame(width: 3, height: 17)
+            Text(workTitle)
+                .font(.callout)
+                .fontWeight(.bold)
+                .foregroundColor(.fontBlack)
+        }.padding(.bottom, 20)
+    }
+    
+    func WorkSpaceCardContents(model: CustomModel) -> some View {
+        ZStack{
+//            RoundedRectangle(cornerRadius: 20)
+//                .fill(Color.green)
+//                .frame(width: .infinity, height: 213)
+//                .padding()
+            
+            VStack(alignment: .leading, spacing: 0) {
+                WorkSpaceCardHeader(workTitle: model.title)
+                
+                VStack(spacing: 8){
+                    ForEach(WorkSpaceInfo.allCases, id: \.self) { tab in
+                        HStack() {
+                            Text(tab.text)
+                                .font(.subheadline)
+                                .foregroundColor(.fontLightGray)
+                            Spacer()
+                            Text(model.text(info: tab))
+                                .font(.subheadline)
+                                .foregroundColor(.fontBlack)
+                        }
+                    }
+                }
+            }
+            .padding()
+            .background(RoundedRectangle(cornerRadius: 10)
+                .fill(Color.))
+            .padding([.horizontal, .bottom])
+        }
+    }
+}
+
+
+
 struct CustomModel: Hashable {
     let title: String
     let money: Int
@@ -88,7 +114,7 @@ struct CustomModel: Hashable {
     let tax: String
     let workType: String
     
-    func text(info: Info) -> String {
+    func text(info: WorkSpaceInfo) -> String {
         switch info {
         case .money: return "\(money)"
         case .date: return date
@@ -109,7 +135,7 @@ let models = [
         workType : "월, 화, 수, 목요일 10:00 - 12:00"
     ),
     CustomModel(
-        title: "팍이네 팍팍 감자탕",
+        title: "팍이네 팍팍팍 감자탕",
         money: 2000,
         date: "2022.07.11",
         pay: "미적용",
@@ -117,7 +143,7 @@ let models = [
         workType : "월, 화, 수, 목요일 10:00 - 12:00"
     ),
     CustomModel(
-        title: "팍이네 팍팍 감자탕",
+        title: "팍이네 팍팍팍팍 감자탕",
         money: 2000,
         date: "2022.07.11",
         pay: "미적용",
