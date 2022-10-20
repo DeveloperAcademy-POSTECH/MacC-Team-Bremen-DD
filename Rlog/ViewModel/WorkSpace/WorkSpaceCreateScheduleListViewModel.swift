b5f44f68c194622b59cb5d78fa25f60595cfc384
song-cool <oscar2978@naver.com>
2022-10-20 11:48:22 +0900

    feat : 뷰모델 생성

---
+++ Rlog/ViewModel/WorkSpace/WorkSpaceCreateScheduleListViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  WorkSpaceCreateScheduleListViewModel.swift
+//  Rlog
+//
+//  Created by 송시원 on 2022/10/20.
+//
+
+import SwiftUI
+
+struct WorkSpaceCreateScheduleListViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
