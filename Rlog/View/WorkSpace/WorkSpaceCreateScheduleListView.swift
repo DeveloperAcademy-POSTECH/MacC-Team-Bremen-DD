06f033498dc25d1e2996c222c3f32b91d8ccf65e
song-cool <oscar2978@naver.com>
2022-10-20 11:36:13 +0900

    feat : 근무 유형 리스트 뷰 만들기
    BD-26
    TODO
    - [ ] 컨포넌트 적용하기
    - [ ] 컨포넌트의 프로퍼티에 따라 vm에 리스트 만들것 구상하기

--- Rlog/View/WorkSpace/WorkSpaceCreateScheduleListView.swift
+++ Rlog/View/WorkSpace/WorkSpaceCreateScheduleListView.swift
@@ -16,6 +16,20 @@ struct WorkSpaceCreateScheduleListView: View {
             //타이틀 섭뷰 넣기 "근무 일정을 입력해주세요." Or "근무 유형을 추가해주세요!"
             Text("sample")
             labelText
+            VStack(spacing: 16) {
+                //리스트 불러와서 반복문으로 돌리기
+                ForEach(1 ..< 3, id: \.self) { s in
+                    //컨포넌트로 대체하기
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(maxHeight: 56)
+                }
+                //컨포넌트로 대체하기
+                Button {} label: {
+                    Text("+ 근무 일정 추가하기")
+                }
+            }
+            Spacer()
+            
         }
         .padding()
     }
