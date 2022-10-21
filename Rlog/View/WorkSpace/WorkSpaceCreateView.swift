8799ad4eb66e6da65bd5139ea6fb0cba94e56659
song-cool <oscar2978@naver.com>
2022-10-21 14:29:04 +0900

    feat : UI 화면 작성 wip

--- Rlog/View/WorkSpace/WorkSpaceCreateView.swift
+++ Rlog/View/WorkSpace/WorkSpaceCreateView.swift
@@ -7,9 +7,50 @@
 
 import SwiftUI
 
+
 struct WorkSpaceCreateView: View {
+    @ObservedObject var viewModel = WorkSpaceCreateViewModel()
+    @State var writingState: WritingState = .hourlyWage
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 20) {
+            guidingTitle
+            guidingText
+            // TODO: 컨포넌트로 대체
+            VStack{
+                Text("근무지")
+                //                TextField(<#T##SwiftUI.LocalizedStringKey#>, text: <#T##SwiftUI.Binding<String>#>, onEditingChanged: <#T##(Bool) -> Void#>)
+            }
+            // TODO: 컨포넌트로 대체
+            Button {
+                print(viewModel.currentState.rawValue)
+                viewModel.isTappedConfirmButton = true
+            } label: {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(height: 58)
+                    Text("확인")
+                }
+            }
+            Spacer()
+        }
+        .padding()
+        
+    }
+}
+
+private extension WorkSpaceCreateView {
+    var guidingTitle: some View {
+        Text("새로운 아르바이트를 추가합니다.")
+            .font(.title2)
+            .fontWeight(.bold)
+            .foregroundColor(.fontBlack)
+        //            .isHidden(true)
+    }
+    var guidingText: some View {
+        Text(viewModel.currentState.title)
+            .font(.title3)
+        
     }
 }
 
--- Rlog/ViewModel/WorkSpace/WorkSpaceCreateViewModel.swift
+++ Rlog/ViewModel/WorkSpace/WorkSpaceCreateViewModel.swift
@@ -5,10 +5,37 @@
 //  Created by 송시원 on 2022/10/17.
 //
 
+import Combine
 import SwiftUI
 
-struct WorkSpaceCreateViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+final class WorkSpaceCreateViewModel: ObservableObject {
+    @Published var currentState: WritingState = .workSpace
+    @Published var isTappedConfirmButton: Bool = false
+    
+    init() {
+        $isTappedConfirmButton.filter {
+            $0 == true
+        }.sink { [weak self] _ in
+            guard let self = self else { return }
+            self.currentState = WritingState(rawValue: self.currentState.rawValue + 1) ?? .workSpace
+        }
+    }
+}
+
+enum WritingState: Int {
+    case workSpace, hourlyWage, payday, toggleOptions
+    
+    var title: String {
+        switch self {
+        case .workSpace:
+            return "근무지를 입력해주세요."
+        case .hourlyWage:
+            return "시급을 입력해주세요."
+        case .payday:
+            return "급여일을 알려주세요."
+        case .toggleOptions:
+            return "소득세, 주휴수당 정보를 입력해주세요."
+        }
     }
 }
