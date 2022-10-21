9fb54b5c2e9fa079a030694278821ba7f8af3d14
Phainestha1 <250wons@gmail.com>
2022-10-21 23:33:01 +0900

    refactor : BD-12 ButtonStyle을 통해 버튼의 활성화를 관리합니다.

--- Rlog/View/Components/CompleteButtonView.swift
+++ Rlog/View/Components/CompleteButtonView.swift
@@ -8,40 +8,42 @@
 import SwiftUI
 
 struct CompleteButtonView: View {
-    var isAvailable: Bool
     let action: () -> Void
     
-    init(isAvailable: Bool, action: @escaping () -> Void) {
-        self.isAvailable = isAvailable
+    init(action: @escaping () -> Void) {
         self.action = action
     }
-
+    
     var body: some View {
-        Button {
-            action()
-        } label: {
-            completeButtonContainerView
-        }
-        .disabled(!isAvailable)
-    }
-
-}
-
-extension CompleteButtonView {
-    var completeButtonContainerView: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 10)
-                .frame(maxWidth: .infinity, minHeight: 56, maxHeight: 56)
-                .foregroundColor(isAvailable ? .primary : .fontLightGray)
-
-            completeButtonLabel
-        }
+        Button("확인") { action() }
+            .buttonStyle(CustomButtonStyle())
     }
     
-    var completeButtonLabel: some View {
-        Text("확인")
-            .font(.body)
-            .foregroundColor(.white)
-            .fontWeight(isAvailable ? .none : .bold)
+    private struct CustomButtonStyle: ButtonStyle {
+        func makeBody(configuration: Configuration) -> some View {
+            StyleButton(configuration: configuration)
+        }
+        
+        private struct StyleButton: View {
+            @Environment(\.isEnabled) var isEnabled
+            
+            let configuration: CustomButtonStyle.Configuration
+            var backgroundColor: Color {
+                if configuration.isPressed {
+                    return .primary.opacity(0.5)
+                }
+                return isEnabled ? .primary : .fontLightGray
+            }
+            
+            var body: some View {
+                    configuration.label
+                        .font(.body)
+                        .foregroundColor(.white)
+                        .fontWeight(isEnabled ? .none : .bold)
+                        .frame(maxWidth: .infinity, minHeight: 56, maxHeight: 56)
+                        .background(backgroundColor)
+                        .cornerRadius(10)
+            }
+        }
     }
 }
