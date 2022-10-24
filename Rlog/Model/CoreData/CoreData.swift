fa5c2fb78e7ad0140eccb76ac8d6a0aade606ac7
insub4067 <vb15@kakao.com>
2022-10-24 18:37:48 +0900

    feat : ScheduleEntity 추가

---
+++ Rlog/Model/CoreData/CoreData.swift
@@ -0,0 +1,16 @@
+//
+//  CoreData.swift
+//  Rlog
+//
+//  Created by Kim Insub on 2022/10/24.
+//
+
+import Foundation
+
+import Foundation
+
+enum CoreData {
+    static let groupIdentifier = "group.com.Bremen-DD.Rlog"
+    static let databaseName = "DataModel.sqlite"
+    static let containerName = "DataModel"
+}
