ac41695c021d5c0e5c9e9857c98b60ebd9c422ad
phainestha1 <250wons@gmail.com>
2022-10-26 02:07:55 +0900

    feat : BD-68 WIPView 파일을 생성합니다.

---
+++ Rlog/View/MonthlyCalculate/WIPView.swift
@@ -0,0 +1,20 @@
+//
+//  WIPView.swift
+//  Rlog
+//
+//  Created by Noah's Ark on 2022/10/26.
+//
+
+import SwiftUI
+
+struct WIPView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WIPView_Previews: PreviewProvider {
+    static var previews: some View {
+        WIPView()
+    }
+}
