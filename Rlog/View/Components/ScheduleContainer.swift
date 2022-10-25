56b93dae2981fa502084ea0ae0654a59f9dfdd18
Phainestha1 <250wons@gmail.com>
2022-10-25 20:54:01 +0900

    feat : BD-74 ScheduleContainer를 구현합니다.

---
+++ Rlog/View/Components/ScheduleContainer.swift
@@ -0,0 +1,74 @@
+//
+//  ScheduleContainer.swift
+//  Rlog
+//
+//  Created by Noah's Ark on 2022/10/25.
+//
+
+import SwiftUI
+
+struct ScheduleContainer: View {
+    let repeatedSchedule: [String]
+    let startHour: String
+    let startMinute: String
+    let endHour: String
+    let endMinute: String
+    
+    var body: some View {
+        scheduleContainerView
+    }
+}
+
+private extension ScheduleContainer {
+    var scheduleContainerView: some View {
+        RoundedRectangle(cornerRadius: 10)
+            .frame(maxWidth: .infinity, minHeight: 56, maxHeight: 56)
+            // TODO: lightGray 세팅 필요
+            .foregroundColor(.gray)
+            .overlay { scheduleInformationView }
+    }
+    
+    var scheduleInformationView: some View {
+        HStack {
+            repeatedScheduleView
+            
+            Spacer()
+            
+            workHourView
+        }
+        .padding()
+    }
+    
+    var repeatedScheduleView: some View {
+        HStack {
+            ForEach(repeatedSchedule, id: \.self) { day in
+                Text(day)
+            }
+        }
+    }
+    
+    var workHourView: some View {
+        HStack {
+            // 시간이 n시 0분인 경우 두 자릿수인 00으로 표시
+            if startMinute == "0" || startMinute == "00" { Text("\(startHour):00") }
+            else { Text("\(startHour):\(startMinute)") }
+            
+            Text("-")
+            
+            if endMinute == "0" || endMinute == "00" { Text("\(endHour):00") }
+            else { Text("\(endHour):\(endMinute)") }
+        }
+    }
+}
+
+//struct ScheduleContainer_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ScheduleContainer(
+//            repeatedSchedule: ["월", "화", "수", "목", "금"],
+//            startHour: 9,
+//            startMinute: 30,
+//            endHour: 18,
+//            endMinute: 0
+//        )
+//    }
+//}
