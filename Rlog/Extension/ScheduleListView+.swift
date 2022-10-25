955c5e91203cd382880984d5fb6e2c4b50917f9c
jayden000106 <cars0106@icloud.com>
2022-10-25 17:19:49 +0900

    feat : BD-36 func에서 property로 수정

--- Rlog/Extension/ScheduleListView+.swift
+++ Rlog/Extension/ScheduleListView+.swift
@@ -49,20 +49,22 @@ extension ScheduleListView {
         }
     }
     
-    // TODO: - 근무지 모델 만들어서, ScheduleCell view model을 만들고, 거기서 값을 가져오는 방법을 사용할 예정
     struct ScheduleCell: View {
+        // TODO: - 근무지 모델 만들어서, ScheduleCell view model을 만들고, 거기서 값을 가져오는 방법을 사용할 예정
+        var isShow = true
+        
         var body: some View {
             ZStack {
                 // TODO: - 조건에 따른 색깔 처리(ViewModel 예정)
-                createBackgroundRectangle(workspaceColor: "PointRed", isShow: true)
-                VStack {
-                    createCellHeader(workspaceColor: "PointRed", workspaceName: "제이든의 낚시교실", spendHour: 4)
+                backgroundRectangle
+                VStack(spacing: 0) {
+                    cellHeader
                     Spacer()
                     HStack(alignment: .bottom, spacing: 0) {
                         // TODO: - 시작 시간, 끝난 시간 처리
-                        createCellContent(monthInt: 10, dayInt: 8, startTime: "17", endTime: "21")
+                        cellContent
                         Spacer()
-                        createCellButton(workspaceColor: "PointRed", isShow: true)
+                        cellButton
                     }
                     .padding(.bottom, -5)
                     .padding(.leading, 14)
@@ -71,11 +73,11 @@ extension ScheduleListView {
             }
         }
         
-        func createBackgroundRectangle(workspaceColor: String, isShow: Bool) -> some View {
+        var backgroundRectangle: some View {
             ZStack {
                 RoundedRectangle(cornerRadius: 10)
                     // TODO: - view model에서 처리
-                    .fill(isShow ? Color(workspaceColor) : .white)
+                    .fill(isShow ? Color("PointRed") : .white)
                     .frame(height: 97)
                 RoundedRectangle(cornerRadius: 9)
                     .fill(Color.white)
@@ -83,40 +85,40 @@ extension ScheduleListView {
             }
         }
         
-        func createCellHeader(workspaceColor: String, workspaceName: String, spendHour: Int) -> some View {
+        var cellHeader: some View {
             HStack(spacing: 0) {
                 Rectangle()
-                    .fill(Color(workspaceColor))
+                    .fill(Color("PointRed"))
                     .frame(width: 3, height: 17)
                 // TODO: - 근무지명 받아오기
-                Text(workspaceName)
+                Text("제이든의 낚시교실")
                     .font(.callout)
                     .fontWeight(.bold)
                     .foregroundColor(Color.fontBlack)
                     .padding(.leading, 3)
                 Spacer()
                 // TODO: - 근무 시간 받아오기
-                Text("\(spendHour)시간")
+                Text("\(4)시간")
                     .font(.subheadline)
                     .fontWeight(.bold)
                     .foregroundColor(Color.fontBlack)
             }
         }
         
-        func createCellContent(monthInt: Int, dayInt: Int, startTime: String, endTime: String) -> some View {
+        var cellContent: some View {
             VStack(alignment: .leading, spacing: 0) {
                 // TODO: - 요일 처리
-                Text("\(monthInt)월 \(dayInt)일 금요일")
+                Text("\(10)월 \(8)일 금요일")
                     .font(.caption)
                     .foregroundColor(Color.fontBlack)
                 // TODO: - 시간 처리
-                Text("\(startTime)시 00분 ~ \(endTime)시 00분")
+                Text("17시 00분 ~ 21시 00분")
                     .font(.caption2)
                     .foregroundColor(Color.fontLightGray)
             }
         }
         
-        func createCellButton(workspaceColor: String, isShow: Bool) -> some View {
+        var cellButton: some View {
             HStack(spacing: 11) {
                 Button(action: {
                     // TODO: - 모달 구현
@@ -137,7 +139,7 @@ extension ScheduleListView {
                         ZStack {
                             RoundedRectangle(cornerRadius: 11)
                             // TODO: - workspace에 맞는 컬러 수정
-                                .fill(Color(workspaceColor))
+                                .fill(Color("PointRed"))
                                 .frame(width: 41, height: 22)
                             Text("확인")
                                 .font(.footnote)
