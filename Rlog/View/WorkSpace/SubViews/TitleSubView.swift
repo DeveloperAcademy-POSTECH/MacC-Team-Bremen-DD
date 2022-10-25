cacd0ba5f001c84f0387e04fbb2d7d109261599e
song-cool <oscar2978@naver.com>
2022-10-25 17:32:55 +0900

    fix : 브랜치(BD-28) 마이그레이션
    BD-28 브랜치의 프로젝트 파일 충돌로, 데이터를 모두 옮겼습니다.

---
+++ Rlog/View/WorkSpace/SubViews/TitleSubView.swift
@@ -0,0 +1,22 @@
+//
+//  TitleSubView.swift
+//  Rlog
+//
+//  Created by 송시원 on 2022/10/19.
+//
+import SwiftUI
+
+struct TitleSubView: View {
+    let title: String
+    var body: some View {
+        Text(title)
+            .font(.title3)
+            .padding(.bottom,20)
+    }
+}
+
+struct TitleSubView_Previews: PreviewProvider {
+    static var previews: some View {
+        TitleSubView(title: "test")
+    }
+}
