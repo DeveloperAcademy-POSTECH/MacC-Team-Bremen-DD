91b1af3d35f3cc1969bd419e603b371736f100cc
insub4067 <vb15@kakao.com>
2022-10-26 13:12:30 +0900

    refactor : BD-73 view 분리

--- Rlog/View/WorkSpace/WorkSpaceListView.swift
+++ Rlog/View/WorkSpace/WorkSpaceListView.swift
@@ -17,7 +17,7 @@ struct WorkSpaceListView: View {
         NavigationView {
             ScrollView {
                 ForEach(viewModel.workspaces, id: \.self) { workspace in
-                    makeWorkspaceCell(workspace: workspace)
+                    WorkSpaceCell(workspace: workspace)
                 }
             }
             .padding(.top, 32)
@@ -48,57 +48,6 @@ struct WorkSpaceListView: View {
         }
 
     }
-}
-
-extension WorkSpaceListView {
-
-    @ViewBuilder
-    func makeWorkspaceCell(workspace: WorkspaceEntity) -> some View {
-        NavigationLink {
-            WorkSpaceDetailView(workspace: workspace)
-        } label: {
-            makeWorkSpaceCardContent(workspace: workspace)
-        }
-    }
-
-    @ViewBuilder
-    func makeWorkSpaceCardHeader(workTitle: String, workTagColor: String) -> some View {
-        HStack(alignment: .center){
-            Rectangle()
-                .foregroundColor(Color(workTagColor))
-                .frame(width: 3, height: 17)
-            Text(workTitle)
-                .font(.callout)
-                .fontWeight(.bold)
-                .foregroundColor(.fontBlack)
-        }.padding(.bottom, 20)
-    }
-
-    @ViewBuilder
-    func makeWorkSpaceCardContent(workspace: WorkspaceEntity) -> some View {
-        VStack(alignment: .leading, spacing: 0) {
-            makeWorkSpaceCardHeader(workTitle: workspace.name, workTagColor: workspace.colorString)
-
-            VStack(spacing: 8){
-                ForEach(WorkSpaceInfo.allCases, id: \.self) { tab in
-                    HStack() {
-                        Text(tab.text)
-                            .font(.subheadline)
-                            .foregroundColor(.fontLightGray)
-                        Spacer()
-                        //                        Text( model.getValue(info: tab))
-                        //                            .font(.subheadline)
-                        //                            .foregroundColor(.fontBlack)
-                    }
-                }
-            }
-        }
-        .padding()
-        .background(RoundedRectangle(cornerRadius: 10)
-            .fill(Color.background))
-        .padding([.horizontal, .bottom])
-    }
-
 }
 
 // TODO: CoreData의 Model로 변경
