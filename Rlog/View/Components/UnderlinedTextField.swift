5e61e54af0da9368ff74bf446d5ae01f7aff65ce
Phainestha1 <250wons@gmail.com>
2022-11-07 17:04:08 +0900

    feat : BD-107 텍스트 필드 구조를 15.0 버젼으로 통일합니다.

--- Rlog/View/Components/UnderlinedTextField.swift
+++ Rlog/View/Components/UnderlinedTextField.swift
@@ -58,134 +58,67 @@ struct UnderlinedTextField: View {
 }
 
 private extension UnderlinedTextField {
-    @ViewBuilder
-    var textFieldView: some View {
-        switch textFieldType {
-        case .workplace, .reason, .none:
-            underlinedTextFieldView
-        case .payday, .wage, .time:
-            underlinedNumberFieldView
-        }
-    }
-
-    var underlinedNumberFieldView: some View {
+    var underlinedTextFieldView: some View {
         VStack {
             HStack {
                 TextField(
                     textFieldType.placeholder,
-                    value: $text,
-                    formatter: NumberFormatter()
+                    text: $text
                 )
                 .padding(.horizontal)
                 .keyboardType(textFieldType.keyboardType)
                 .focused($isNumberFieldFocused)
+                .onChange(of: text) { newValue in
+                    switch textFieldType {
+                    case .workplace:
+                        if text.count == 20 { maximumText = newValue }
+                        if newValue.count > 20 { text = maximumText }
+                    case .wage:
+                        guard let textToInt = Int(newValue) else { return text = "" }
+                        if newValue.hasPrefix("0") { text = "" }
+                        if newValue.count == 7 { maximumText = newValue }
+                        if textToInt >= 1000000 { text = maximumText }
+                    case .payday:
+                        guard let textToInt = Int(newValue) else { return text = "" }
+                        if newValue.hasPrefix("0") { text = "" }
+                        if text.count == 2 { maximumText = newValue }
+                        if textToInt > 28 || textToInt < 1 { text = maximumText }
+                    case .reason, .time, .none:
+                        return
+                    }
+                }
 
                 Spacer()
 
                 if textFieldType == .workplace {
-                    HStack {
-                        Spacer()
-                        Text("\(String(describing: text).count)/20")
-                            .font(.caption)
-                            .foregroundColor(.gray)
-                    }
-                    .padding(.trailing)
+                    Text("\(String(describing: text).count)/20")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                        .padding(.trailing)
                 }
             }
 
             Rectangle()
                 .frame(maxWidth: .infinity, minHeight: 2, maxHeight: 2)
-                .foregroundColor(isNumberFieldFocused == false ? .gray : .green)
-        }
-        .frame(maxWidth: .infinity, minHeight: 30, maxHeight: 30)
-
-    }
-    var underlinedTextFieldView: some View {
-        VStack {
-            HStack {
-                UITextFieldRepresentable(
-                    text: $text,
-                    placeholder: textFieldType.placeholder,
-                    isFocused: $isFocused
-                )
-                .padding(.horizontal)
-                .keyboardType(textFieldType.keyboardType)
-
-                Spacer()
-
-                if textFieldType == .workplace {
-                    HStack {
-                        Spacer()
-                        Text("\(String(describing: text).count)/20")
-                            .font(.caption)
-                            .foregroundColor(.gray)
-                    }
-                    .padding(.trailing)
-                }
-            }
-
-            Rectangle()
-                .frame(maxWidth: .infinity, minHeight: 2, maxHeight: 2)
-                .foregroundColor(isFocused == false ? .gray : .green)
+                .foregroundColor(isNumberFieldFocused == false ? .gray : isErrorOnTextField())
         }
         .frame(maxWidth: .infinity, minHeight: 30, maxHeight: 30)
     }
-}
-
-// https://medium.com/hcleedev/swift-textfield-기존-focus-방식과-새롭게-등장한-focusstate-활용하기-8725c7425140
-private struct UITextFieldRepresentable<T>: UIViewRepresentable {
-    @Binding var text: T
-    var placeholder: String
-    var isFirstResponder = false
-    @Binding var isFocused: Bool
-
-    func makeUIView(context: UIViewRepresentableContext<UITextFieldRepresentable>) -> UITextField {
-        let textField = UITextField(frame: .zero)
-        textField.delegate = context.coordinator
-        textField.placeholder = self.placeholder
-
-        return textField
-    }
-
-    func updateUIView(_ uiView: UITextField, context: UIViewRepresentableContext<UITextFieldRepresentable>) {
-        uiView.text = String(describing: self.text)
-        if isFirstResponder && !context.coordinator.didFirstResponder {
-            uiView.becomeFirstResponder()
-            context.coordinator.didFirstResponder = true
+    
+    func isErrorOnTextField() -> Color {
+        switch textFieldType {
+        case .workplace:
+            if text.count == 20 { return .red }
+        case .wage:
+            guard let textToInt = Int(text) else { return .red }
+            if textToInt >= 1000000 { return .red }
+        case .payday:
+            guard let textToInt = Int(text) else { return .red }
+            if textToInt > 28 || textToInt < 1 { return .red }
+        case .reason, .time, .none:
+            return .primary
         }
-    }
-
-    func makeCoordinator() -> UITextFieldRepresentable.Coordinator {
-        Coordinator(text: self.$text, isFocused: self.$isFocused)
-    }
-
-    class Coordinator: NSObject, UITextFieldDelegate {
-        @Binding var text: T
-        @Binding var isFocused: Bool
-        var didFirstResponder = false
-
-        init(text: Binding<T>, isFocused: Binding<Bool>) {
-            self._text = text
-            self._isFocused = isFocused
-        }
-
-        func textFieldDidChangeSelection(_ textField: UITextField) {
-            guard let genericText = textField.text as? T else { return }
-            self.text = genericText
-        }
-
-        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-            textField.resignFirstResponder()
-            return true
-        }
-
-        func textFieldDidBeginEditing(_ textField: UITextField) {
-            self.isFocused = true
-        }
-
-        func textFieldDidEndEditing(_ textField: UITextField) {
-            self.isFocused = false
-        }
+        
+        return .primary
     }
 }
-
