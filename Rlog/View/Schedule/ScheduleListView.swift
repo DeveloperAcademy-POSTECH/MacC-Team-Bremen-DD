1816fb3be97a85bc4e3a7bb156170c7b1eca41c8
phainestha1 <250wons@gmail.com>
2022-11-10 23:43:11 +0900

    refactor: 불필요한 변경을 초기화 합니다.

--- Rlog/View/Schedule/ScheduleListView.swift
+++ Rlog/View/Schedule/ScheduleListView.swift
@@ -9,12 +9,6 @@ import SwiftUI
 
 struct ScheduleListView: View {
     var body: some View {
-        VStack {
-            InputFormElement(containerType: .payday, text: .constant("25"))
-            InputFormElement(containerType: .wage, text: .constant("25"))
-            InputFormElement(containerType: .workplace, text: .constant("25"))
-            InputFormElement(containerType: .reason, text: .constant("25"))
-        }
-        .padding(.horizontal)
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
