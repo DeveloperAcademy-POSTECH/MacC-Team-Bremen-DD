a0bb6e8cde1db1a480adaa13defdd6fa2ef0482d
phainestha1 <250wons@gmail.com>
2022-11-11 00:20:15 +0900

    feat : BD-124 메인뷰의 스크롤 캘린더 UI를 구현합니다.

---
+++ Rlog/Extension/View+.swift
@@ -0,0 +1,26 @@
+//
+//  View+.swift
+//  Rlog
+//
+//  Created by Noah's Ark on 2022/11/10.
+//
+
+import Foundation
+import SwiftUI
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, _ corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
+
+// 도형의 일부분에 곡선 적용하기
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
