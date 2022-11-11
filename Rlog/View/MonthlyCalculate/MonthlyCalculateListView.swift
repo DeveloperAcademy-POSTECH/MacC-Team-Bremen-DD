3f2718f90d2f8e6cf00923b431c5e47f72bd79e5
jayden000106 <cars0106@icloud.com>
2022-11-11 17:06:18 +0900

    feat: BD-23 상단 헤더 구현
    - 타이틀(정산)과 월 표시부 구현

--- Rlog/View/MonthlyCalculate/MonthlyCalculateListView.swift
+++ Rlog/View/MonthlyCalculate/MonthlyCalculateListView.swift
@@ -9,6 +9,39 @@ import SwiftUI
 
 struct MonthlyCalculateListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+            header
+                .padding(.top, 24)
+            Spacer()
+        }
+        .padding(.horizontal)
+    }
+}
+
+private extension MonthlyCalculateListView {
+    var header: some View {
+        HStack {
+            Text("정산")
+                .font(.title2)
+                .fontWeight(.bold)
+            Spacer()
+            HStack(spacing: 8) {
+                Button(action: {
+                    // TODO: - ViewModel에서 로직 구현
+                }, label: {
+                    Image(systemName: "chevron.left")
+                })
+                // TODO: - 현재 연도, 월로 바꾸기
+                Text("2022.11")
+                    .fontWeight(.bold)
+                Button(action: {
+                    // TODO: - ViewModel에서 로직 구현
+                }, label: {
+                    Image(systemName: "chevron.right")
+                })
+            }
+            .font(.title)
+            .foregroundColor(.black)
+        }
     }
 }
