256a5347add5b96b6c5557d4080d553efc4dd456
jayden000106 <cars0106@icloud.com>
2022-11-14 10:06:56 +0900

    feat: BD-140 상단 근무지 정보 헤더 구현

--- Rlog/View/MonthlyCalculate/MonthlyCalculateDetailView.swift
+++ Rlog/View/MonthlyCalculate/MonthlyCalculateDetailView.swift
@@ -9,7 +9,26 @@ import SwiftUI
 
 struct MonthlyCalculateDetailView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 0) {
+            header
+        }
+    }
+}
+
+private extension MonthlyCalculateDetailView {
+    var header: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text("GS25 포항공대점")
+                .font(.title2)
+                .fontWeight(.bold)
+                .foregroundColor(Color.fontBlack)
+            Text("2022년 10월 25일 ~ 2022년 11월 24일")
+                .font(.subheadline)
+                .foregroundColor(Color.fontBlack)
+            Text("정산일까지 D-12")
+                .font(.caption2)
+                .foregroundColor(Color.pointRed)
+        }
     }
 }
 
