401507b11f21a2e321e47c1bbb9f63b5e660fbf3
insub4067 <vb15@kakao.com>
2022-11-14 14:49:35 +0900

    feat : BD-170 - ScheduleEntity CRUD 로직 작성

--- Rlog/CoreData/CoreDataManager.swift
+++ Rlog/CoreData/CoreDataManager.swift
@@ -82,3 +82,52 @@ extension CoreDataManager {
         save()
     }
 }
+
+// MARK: - ScheduleEntity Logic
+extension CoreDataManager {
+    func createSchedule(
+        of workspace: WorkspaceEntity,
+        repeatDays: [String],
+        startHour: Int16,
+        startMinute: Int16,
+        endHour: Int16,
+        endMinute: Int16
+    ) {
+        let schedule = ScheduleEntity(context: context)
+        schedule.workspace = workspace
+        schedule.repeatDays = repeatDays
+        schedule.startHour = startHour
+        schedule.startMinute = startMinute
+        schedule.endHour = endHour
+        schedule.endMinute = endMinute
+        save()
+    }
+
+    func getSchedules(of workspace: WorkspaceEntity) -> [ScheduleEntity] {
+        let fetchRequest: NSFetchRequest<ScheduleEntity> = ScheduleEntity.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "workspace.name = %@", workspace.name)
+        let result = try? context.fetch(fetchRequest)
+        return result ?? []
+    }
+
+    func editSchedule(
+        of schedule: ScheduleEntity,
+        repeatDays: [String],
+        startHour: Int16,
+        startMinute: Int16,
+        endHour: Int16,
+        endMinute: Int16
+    ) {
+        schedule.repeatDays = repeatDays
+        schedule.startHour = startHour
+        schedule.startMinute = startMinute
+        schedule.endHour = endHour
+        schedule.endMinute = endMinute
+        save()
+    }
+
+    func deleteSchedule(of schedule: ScheduleEntity) {
+         context.delete(schedule)
+         save()
+     }
+}
