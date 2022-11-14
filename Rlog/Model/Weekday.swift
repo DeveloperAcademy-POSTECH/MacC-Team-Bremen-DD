c7455b89c011237f46e254a74b0042440198ea8a
Hyeon-sang "Theo" Lee <88080251+phainestha1@users.noreply.github.com>
2022-11-14 12:21:23 +0900

    Update Rlog/Model/Weekday.swift
    Co-authored-by: songcool <46256034+song-cool@users.noreply.github.com>

--- Rlog/Model/Weekday.swift
+++ Rlog/Model/Weekday.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 enum Weekday: String, CaseIterable {
+    case sun = "SUN"
     case mon = "MON"
     case tue = "TUE"
     case wed = "WED"
     case thu = "THU"
     case fri = "FRI"
     case sat = "SAT"
-    case sun = "SUN"
 }
