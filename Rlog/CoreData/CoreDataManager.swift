b9f7f4cd6c064cc66e12491fc22e8cbd061f6926
insub4067 <vb15@kakao.com>
2022-11-14 14:37:04 +0900

    feat : BD-169 - WorkspaceEntity CRUD 로직 작성

--- Rlog/CoreData/CoreDataManager.swift
+++ Rlog/CoreData/CoreDataManager.swift
@@ -27,14 +27,58 @@ struct CoreDataManager {
               }
         }
     }
-}
 
-extension CoreDataManager {
     private func save() {
         do {
             try context.save()
         } catch {
             print("FAILED TO SAVE CONTEXT")
         }
+    }
+}
+
+// MARK: - Workspace Logic
+extension CoreDataManager {
+    func createWorkspace(
+        name: String,
+        payDay: Int16,
+        hourlyWage: Int32,
+        hasTax: Bool,
+        hasJuhyu: Bool
+    ) {
+        let workspace = WorkspaceEntity(context: context)
+        workspace.name = name
+        workspace.payDay = payDay
+        workspace.hourlyWage = hourlyWage
+        workspace.hasTax = hasTax
+        workspace.hasJuhyu = hasJuhyu
+        save()
+    }
+
+    func getAllWorkspaces() -> [WorkspaceEntity] {
+        let fetchRequest: NSFetchRequest<WorkspaceEntity> = WorkspaceEntity.fetchRequest()
+        let result = try? context.fetch(fetchRequest)
+        return result ?? []
+    }
+
+    func editWorkspace(
+        workspace: WorkspaceEntity,
+        name: String,
+        payDay: Int16,
+        hourlyWage: Int32,
+        hasTax: Bool,
+        hasJuhyu: Bool
+    ) {
+        workspace.name = name
+        workspace.payDay = payDay
+        workspace.hourlyWage = hourlyWage
+        workspace.hasTax = hasTax
+        workspace.hasJuhyu = hasJuhyu
+        save()
+    }
+
+    func deleteWorkspace(workspace: WorkspaceEntity) {
+        context.delete(workspace)
+        save()
     }
 }
