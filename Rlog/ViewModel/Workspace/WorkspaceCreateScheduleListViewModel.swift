64880ad26599ab894b2277f3a27cc00ad05796b1
Phainestha1 <250wons@gmail.com>
2022-11-21 16:56:57 +0900

    refactor: BD-203 파일명을 변경합니다.

---
+++ Rlog/ViewModel/Workspace/WorkspaceCreateScheduleListViewModel.swift
@@ -0,0 +1,63 @@
+//
+//  WorkSpaceCreateScheduleListViewModel.swift
+//  Rlog
+//
+//  Created by 송시원 on 2022/10/20.
+//
+
+import SwiftUI
+
+@MainActor
+final class WorkspaceCreateScheduleListViewModel: ObservableObject {
+    @Binding var isActiveNavigation: Bool
+    var workspaceModel: WorkSpaceModel
+    init(isActiveNavigation: Binding<Bool>, workspaceModel: WorkSpaceModel) {
+        self._isActiveNavigation = isActiveNavigation
+        self.workspaceModel = workspaceModel
+    }
+    
+    @Published var isShowingModal = false
+    @Published var isDisabledNextButton = false
+    
+    @Published var scheduleList: [ScheduleModel] = [] {
+        didSet {
+            if !scheduleList.isEmpty {
+                isDisabledNextButton = false
+            }
+        }
+    }
+    
+    func didTapAddScheduleButton() {
+        showModal()
+    }
+    func getScheduleData() -> [ScheduleModel] {
+        return scheduleList
+    }
+    func didTapDeleteButton(idx: Int) {
+        scheduleList.remove(at: idx)
+    }
+}
+
+// MARK: - Private Functions
+private extension WorkspaceCreateScheduleListViewModel {
+    func showModal() {
+        isShowingModal = true
+    }
+}
+
+struct ScheduleModel: Hashable{
+    var repeatedSchedule: [String] = []
+    var startHour: String = ""
+    var startMinute: String = ""
+    var endHour: String = ""
+    var endMinute: String = ""
+}
+
+
+struct WorkSpaceModel {
+    var name: String
+    var paymentDay: String
+    var hourlyWage: String
+    var hasTax: Bool
+    var hasJuhyu: Bool
+}
