a411b093fea51216e6f7b7b051f041174f9d0d13
Phainestha1 <250wons@gmail.com>
2022-11-21 17:21:41 +0900

    refactor : BD-203 WorkspaceListView 리팩토링을 진행합니다.

--- Rlog/View/WorkSpace/WorkSpaceListView.swift
+++ Rlog/View/WorkSpace/WorkSpaceListView.swift
@@ -12,38 +12,44 @@ struct WorkSpaceListView: View {
     
     var body: some View {
         NavigationView {
-            
-            VStack(spacing: 0){
-                HStack(){
-                    Text("근무지")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color.fontBlack)
-                    
-                    Spacer()
-                    
-                    NavigationLink(
-                        destination: WorkspaceCreateView(isActive: $viewModel.isShowingSheet),
-                        isActive: $viewModel.isShowingSheet) {
-                            
+            workspaceList
+        }
+    }
+}
+
+private extension WorkSpaceListView {
+    var workspaceList: some View {
+        VStack(spacing: 0){
+            HStack(){
+                Text("근무지")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.fontBlack)
+                
+                Spacer()
+                
+                NavigationLink(
+                    destination: WorkspaceCreateView(isActive: $viewModel.isShowingSheet),
+                    isActive: $viewModel.isShowingSheet) {
+                        
                         Image("plus.curved")
                             .foregroundColor(Color.primary)
                     }
-                }
-                .padding(EdgeInsets(top: 27, leading: 16, bottom: 24, trailing: 16))
-                
-                ScrollView {
-                    ForEach(viewModel.workspaces, id: \.self) { workspace in
-                        WorkspaceCell(workspace: workspace)
-                    }
-                }
             }
-            .navigationBarTitle("", displayMode: .automatic)
-            .navigationBarHidden(true)
-            .background(Color.backgroundWhite)
-            .onAppear {
-                viewModel.onAppear()
+            .padding(EdgeInsets(top: 27, leading: 16, bottom: 24, trailing: 16))
+            
+            ScrollView {
+                ForEach(viewModel.workspaces, id: \.self) { workspace in
+                    WorkspaceCell(workspace: workspace)
+                }
             }
         }
+        .navigationBarTitle("", displayMode: .automatic)
+        .navigationBarHidden(true)
+        .background(Color.backgroundWhite)
+        .onAppear {
+            viewModel.onAppear()
+        }
+        
     }
 }
