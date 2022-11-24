8fdb52f7c4c5d5988c878a3688a59ea4bbabf79f
jayden000106 <cars0106@icloud.com>
2022-11-24 16:17:29 +0900

    feat: BD-10 임시 주휴수당 로직 생성

--- Rlog/ViewModel/MonthlyCalculate/MonthlyCalculateListViewModel.swift
+++ Rlog/ViewModel/MonthlyCalculate/MonthlyCalculateListViewModel.swift
@@ -25,7 +25,9 @@ struct MonthlyCalculateResult {
     var calculateTax: Int {
         return Int(Double(totalWithoutTaxAndJuhu) * 0.033)
     }
-//    var calculateJuhu: Int
+    var calculateJuhu: Int {
+        return calculateJuhuTest()
+    }
     var leftDays: Int {
         return calculateleftDays(workspace: workspace)
     }
@@ -135,6 +137,10 @@ struct MonthlyCalculateResult {
             total -= calculateTax
         }
         return total
+    }
+    
+    private func calculateJuhuTest() -> Int {
+        return Int((Double(workspace.hourlyWage) * 1.2) * Double(workHours) - Double(total))
     }
 }
 
