ff17cd395ccbdc1c811070e133fc938cdbe111be
insub4067 <vb15@kakao.com>
2022-11-28 14:39:08 +0900

    refactor : BD-240 - 일치하는 schedule 찾는  로직 수정

--- Rlog/Manager/WorkTypeManager.swift
+++ Rlog/Manager/WorkTypeManager.swift
@@ -11,24 +11,22 @@ final class WorkTypeManager {
     private let timeManager = TimeManager()
 
     func defineWorkType(workday: WorkdayEntity) -> WorkDayType {
-        if workday.schedule == nil { return .extraDay }
+        guard let relatedSchedule = workday.schedule else { return .extraDay }
 
         let workspace = workday.workspace
         let schedules = CoreDataManager.shared.getSchedules(of: workspace)
 
-        let weekday = timeManager.getWeekdayOfDate(workday.date)
         let workdayStartTime = workday.startTime
         let workdayEndTime = workday.endTime
         let workdaySpentHour = timeManager.calculateTimeGapBetweenTwoDate(start: workdayStartTime, end: workdayEndTime)
 
         for schedule in schedules {
-            let repeatDays = schedule.repeatDays
             let startHour = schedule.startHour
             let startMinute = schedule.startMinute
             let endHour = schedule.endHour
             let endMinute = schedule.endMinute
 
-            guard repeatDays.contains(weekday),
+            guard schedule.objectID == relatedSchedule.objectID,
                   let spentHour = timeManager.calculateTimeGap(
                       startHour: startHour,
                       startMinute: startMinute,
